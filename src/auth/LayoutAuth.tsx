15c3d956510f83fd7f247cfe6ddbea3788d3e91b
Kaiosuke <trongleele@gmail.com>
2025-04-24 16:11:18 +0700

    update back to home layout api

--- src/auth/LayoutAuth.tsx
+++ src/auth/LayoutAuth.tsx
@@ -2,8 +2,9 @@ import ImageSign from "@/assets/images/sign.png";
 import i18n from "@/i18n/i18n";
 import { languageSelector } from "@/redux/selectors/languageSelector";
 import { useEffect } from "react";
+import { FaArrowLeftLong } from "react-icons/fa6";
 import { useSelector } from "react-redux";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 const LayoutAuth = () => {
   const { lg } = useSelector(languageSelector);
@@ -14,6 +15,13 @@ const LayoutAuth = () => {
 
   return (
     <>
+      <Link
+        to="/"
+        className="fixed top-10 left-10 text-primary flex items-center gap-2  text-size-3xl"
+      >
+        <FaArrowLeftLong />
+        Home
+      </Link>
       <div className="grid lg:grid-cols-2 h-screen w-screen grid-cols-1">
         <Outlet />
         <div className="w-full">
