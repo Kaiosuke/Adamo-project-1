a5627168c965c17468536be78cfd94738f489023
Kaiosuke <trongleele@gmail.com>
2025-05-06 11:54:56 +0700

    fix: border-radius map

--- src/components/MapCom.tsx
+++ src/components/MapCom.tsx
@@ -7,12 +7,7 @@ const position: [number, number] = [30.028511, 105.804817]
 function MapCom({ coordinates = position }: { coordinates?: [number, number] }) {
   return (
     <div className="sub-2-container">
-      <MapContainer
-        center={coordinates}
-        zoom={13}
-        style={{ height: '400px', width: '100%', borderRadius: '10px' }}
-        className="rounded-2xl"
-      >
+      <MapContainer center={coordinates} zoom={13} style={{ height: '400px', width: '100%' }}>
         <TileLayer
           attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
