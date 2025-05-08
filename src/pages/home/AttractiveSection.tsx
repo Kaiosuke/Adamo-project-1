0d9ae32243f61319ab7cedc7a1ddbf0472be377e
Kaiosuke <trongleele@gmail.com>
2025-05-08 15:45:23 +0700

    feat: arrow slide

--- src/pages/home/AttractiveSection.tsx
+++ src/pages/home/AttractiveSection.tsx
@@ -1,15 +1,22 @@
 import ButtonFeature from '@components/ButtonFeature'
+
 import Tour from '@components/Tour'
 import { ITour } from '@interfaces/tour'
+import { useRef } from 'react'
+import { Swiper as SwiperType } from 'swiper/types'
 
 import { Trans, useTranslation } from 'react-i18next'
 import { Link } from 'react-router'
 import 'swiper/css'
 import 'swiper/css/pagination'
+import { Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { GrLinkNext } from 'react-icons/gr'
 
 const AttractiveSection = ({ data }: { data: ITour[] }) => {
   const { t } = useTranslation('others')
+
+  const swiperRef = useRef<SwiperType | null>(null)
 
   return (
     <section className="main-container animate-fade-down">
@@ -24,27 +31,33 @@ const AttractiveSection = ({ data }: { data: ITour[] }) => {
         </div>
       </div>
 
-      <Swiper
-        spaceBetween={12}
-        className="mySwiper2 opacity-none mt-4"
-        breakpoints={{
-          640: {
-            slidesPerView: 2
-          },
-          1024: {
-            slidesPerView: 3
-          }
-        }}
-      >
-        <div className="grid lg:grid-cols-3 gap-4 md:grid-cols-2 grid-cols-1 pt-6 opacity-none">
-          {data?.length &&
-            data.map((v) => (
-              <SwiperSlide key={v.id}>
-                <Tour tour={v} />
-              </SwiperSlide>
-            ))}
-        </div>
-      </Swiper>
+      <div className="relative">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={12}
+          className="mySwiper2 mt-4"
+          onSwiper={(swiper: SwiperType) => {
+            swiperRef.current = swiper
+          }}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 }
+          }}
+        >
+          {data?.map((v: ITour) => (
+            <SwiperSlide key={v.id}>
+              <Tour tour={v} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <button
+          onClick={() => swiperRef.current?.slideNext()}
+          className="absolute top-1/3 -right-10 z-50 md:block hidden cursor-pointer"
+        >
+          <GrLinkNext className="text-secondary" />
+        </button>
+      </div>
     </section>
   )
 }
--- src/pages/home/Traditional.tsx
+++ src/pages/home/Traditional.tsx
@@ -2,14 +2,19 @@ import ButtonFeature from '@components/ButtonFeature'
 
 import Tour from '@components/Tour'
 import { ITour } from '@interfaces/tour'
+import { useRef } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
+import { GrLinkNext } from 'react-icons/gr'
 import { Link } from 'react-router'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper as SwiperType } from 'swiper/types'
 
 const TraditionalSection = ({ data }: { data: ITour[] }) => {
   const { t } = useTranslation('others')
+
+  const swiperRef = useRef<SwiperType | null>(null)
 
   return (
     <section className="main-container animate-fade-down">
@@ -23,28 +28,39 @@ const TraditionalSection = ({ data }: { data: ITour[] }) => {
           </Link>
         </div>
       </div>
-      <Swiper
-        spaceBetween={12}
-        className="mySwiper2 opacity-none mt-4"
-        breakpoints={{
-          640: {
-            slidesPerView: 2
-          },
+      <div className="relative">
+        <Swiper
+          spaceBetween={12}
+          className="mySwiper2 opacity-none mt-4"
+          breakpoints={{
+            640: {
+              slidesPerView: 2
+            },
 
-          1024: {
-            slidesPerView: 3
-          }
-        }}
-      >
-        <div className="grid lg:grid-cols-3 gap-4 md:grid-cols-2 grid-cols-1 pt-6 opacity-none">
-          {data?.length &&
-            data.map((v) => (
-              <SwiperSlide key={v.id}>
-                <Tour tour={v} />
-              </SwiperSlide>
-            ))}
-        </div>
-      </Swiper>
+            1024: {
+              slidesPerView: 3
+            }
+          }}
+          onSwiper={(swiper: SwiperType) => {
+            swiperRef.current = swiper
+          }}
+        >
+          <div className="grid lg:grid-cols-3 gap-4 md:grid-cols-2 grid-cols-1 pt-6 opacity-none">
+            {data?.length &&
+              data.map((v) => (
+                <SwiperSlide key={v.id}>
+                  <Tour tour={v} />
+                </SwiperSlide>
+              ))}
+          </div>
+        </Swiper>
+        <button
+          onClick={() => swiperRef.current?.slideNext()}
+          className="absolute top-1/3 -right-10 z-50 md:block hidden cursor-pointer"
+        >
+          <GrLinkNext className="text-secondary" />
+        </button>
+      </div>
     </section>
   )
 }
