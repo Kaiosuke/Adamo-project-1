d0a1b7380b806e25b9c82d8777f2053f4e653c48
Kaiosuke <trongleele@gmail.com>
2025-05-09 11:57:40 +0700

    chore: language contact office

--- src/pages/contact/Office.tsx
+++ src/pages/contact/Office.tsx
@@ -10,38 +10,32 @@ const Office = () => {
       <img src={OurOffice} alt="our office" className="w-full h-full object-cover" />
       <div className="md:absolute md:mt-0 mt-6 bottom-0 md:max-w-[422px] max-w-full md:max-h-[324px] max-h-full w-full h-full bg-secondary">
         <div className="lg:p-10 md:p-8 p-6 h-full flex flex-col justify-between">
-          <h3 className="text-size-4xl text-third">{t('contact.ourOffice')}</h3>
+          <h3 className="text-size-4xl text-third">{t('contact.office.title')}</h3>
           <div className="flex flex-col h-full justify-between text-primary mt-6 md:gap-0 gap-8">
             <div className="flex gap-2 items-center">
-              <div>
-                <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
-                  <IoHomeOutline />
-                </div>
+              <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
+                <IoHomeOutline />
               </div>
               <div>
-                <span className="text-size-lg">Address</span>
+                <span className="text-size-lg">{t('contact.office.address')}</span>
                 <div className="text-third text-sm">27 Old Gloucester Street, London, WC1N 3AX</div>
               </div>
             </div>
             <div className="flex gap-2 items-center">
-              <div>
-                <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
-                  <IoHomeOutline />
-                </div>
+              <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
+                <IoHomeOutline />
               </div>
               <div>
-                <span className="text-size-lg">Address</span>
+                <span className="text-size-lg">{t('contact.office.address')}</span>
                 <div className="text-third text-sm">27 Old Gloucester Street, London, WC1N 3AX</div>
               </div>
             </div>
             <div className="flex gap-2 items-center">
-              <div>
-                <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
-                  <IoHomeOutline />
-                </div>
+              <div className="w-[40px] h-[40px] text-third bg-primary flex items-center justify-center rounded-full">
+                <IoHomeOutline />
               </div>
               <div>
-                <span className="text-size-lg">Address</span>
+                <span className="text-size-lg">{t('contact.office.address')}</span>
                 <div className="text-third text-sm">27 Old Gloucester Street, London, WC1N 3AX</div>
               </div>
             </div>
